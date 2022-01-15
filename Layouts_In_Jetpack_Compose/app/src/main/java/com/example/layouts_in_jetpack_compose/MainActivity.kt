4918f59d4f26aa3b3214548127ed6c435e3d734b
이영한 <mashiro@snu.ac.kr>
2022-01-15 23:50:48 +0900

    6-3

--- Layouts_In_Jetpack_Compose/app/src/main/java/com/example/layouts_in_jetpack_compose/MainActivity.kt
+++ Layouts_In_Jetpack_Compose/app/src/main/java/com/example/layouts_in_jetpack_compose/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.layouts_in_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.relocationRequester
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.layouts_in_jetpack_compose.ui.theme.Layouts_In_Jetpack_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +30,38 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun PhotographerCard(modifier : Modifier = Modifier) {
+    Row (modifier){
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            //Images
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisely", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    Layouts_In_Jetpack_ComposeTheme {
+        PhotographerCard()
     }
 }
 
